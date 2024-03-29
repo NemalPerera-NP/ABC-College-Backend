7b069cdf39051e405d3441f3ca12f38682e0cb2d
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-29 23:25:31 +0530

    backecnd server sucessfuly run in directed port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cros = require("cors");
 
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080; //define a port for the server
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
