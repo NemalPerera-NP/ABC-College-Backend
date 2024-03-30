e12f311990e8c311714997d9ece39c63e0730ad9
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-30 10:42:37 +0530

    backend server connection successful

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cros = require("cors");
-
+const mysql = require("mysql");
 
 const app = express();
+app.use(cros());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080; //define a port for the server
 
-
+app.get("/",(req,res)=>{
+  return res.json("From Backend Side Nemal")
+})
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });
+  console.log(`Server is running on port ${PORT}.`);
+});
