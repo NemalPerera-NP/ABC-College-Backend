496dff0b78abe229a8ec6dd410c61809bbb950aa
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-30 11:13:23 +0530

    conection with mysql database established and working correctly

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// DB_NAME=database_name
+// DB_USER=username
+// DB_PASSWORD=password
+// DB_HOST=localhost:3306
+
+const db = mysql.createConnection({
+  host:process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect(error => {
+    if (error) {
+      return console.error('Error connecting to the database: ', error);
+    }
+    console.log('Connected to the MySQL database.');
+  });
+  
+  module.exports = db;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cros = require("cors");
 const mysql = require("mysql");
+const dbConnection = require("./config/db");
 
 const app = express();
 app.use(cros());
@@ -9,9 +10,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080; //define a port for the server
 
-app.get("/",(req,res)=>{
-  return res.json("From Backend Side Nemal")
-})
+//Data Base connection
+
+app.get("/", (req, res) => {
+  return res.json("From Backend Side Nemal");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
