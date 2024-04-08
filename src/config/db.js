69ab7ca5fb7864c05ad4e27b04bd31bba9eb72a2
NemalPerera-NP <nemalperera1999@gmail.com>
2024-04-08 17:34:54 +0530

    backend implementation completed

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,22 @@
-const mysql = require('mysql2/promise');
-//import dotenv from "otenv";
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv").config();
 
 const db = mysql.createPool({
- 
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "abccollege",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
-
 
 (async () => {
   try {
-    const [rows] = await db.query('SELECT 1');
-    console.log('Database connection has been established successfully.');
+    const [rows] = await db.query("SELECT 1");
+    console.log("Database connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
