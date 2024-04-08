86329277f9dcb5a5a32b6cee4ae52c27d2ce3d72
NemalPerera-NP <nemalperera1999@gmail.com>
2024-04-08 14:20:21 +0530

    token integration added to the APIs

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -21,8 +21,12 @@ router.post("/login", loginUserControle);
 router.post("/signup", userRegisterController);
 router.post("/reg-key", userRegistrationKeyCreationControle);
 router.post("/student", studentCreateController);
-router.get("/get-setudents", authenticateToken,getallStudentController);
-router.get("/get-setudents/:id", getStudentByIdController);
-router.put("/update-setudents/:id", updateStudentController);
-router.delete("/delete-setudents/:id", deleteStudentController);
+router.get("/get-setudents", authenticateToken, getallStudentController);
+router.get("/get-setudents/:id", authenticateToken, getStudentByIdController);
+router.put("/update-setudents/:id", authenticateToken, updateStudentController);
+router.delete(
+  "/delete-setudents/:id",
+  authenticateToken,
+  deleteStudentController
+);
 module.exports = router;
